ae3e67f0e20c28d206139379c55ab7453056aa2b
peterermakow <peterinno@yandex.ru>
2024-05-26 15:00:27 +0300

    first commit

---
+++ product_core/src/test/java/ru/ermakow/product_core/ProductCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ermakow.product_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
